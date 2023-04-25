c268f4ac76522e0f684ce8da34d21c32f1de7b03
isa <chomoevisa@gmail.com>
2023-04-25 20:02:14 +0600

    Add file

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -4,6 +4,9 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(label='Название статьи', widget=forms.TextInput())
+    text = forms.CharField(label="Описание статьи", widget=forms.Textarea())
+
     class Meta:
         model = Post
         fields = ('title', 'text',)
