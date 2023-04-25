62cb90cb30ac1929b090cc6233a6b0dc2bec1c6d
isa <chomoevisa@gmail.com>
2023-04-25 19:37:29 +0600

    Add file

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,19 +1,19 @@
-from django.contrib import messages
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-
+from .models import Post
 from .forms import PostForm
-from . import models, forms
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 def post_list(request):
-    posts = models.Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
-    post = get_object_or_404(PostForm, pk=pk)
+    post = get_object_or_404(Post, pk=pk)
 
     return render(request, 'blog/post_detail.html', {'post': post})
 
